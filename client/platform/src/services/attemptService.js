7a193919b2e24382196d2b3b41fb616e66a454da
satvik <satvikwork2004@gmail.com>
2026-05-09 13:47:27 +0530

    fixed review answers

--- client/platform/src/services/attemptService.js
+++ client/platform/src/services/attemptService.js
@@ -1,19 +1,4 @@
-import axios from "axios";
+import api from "../api/axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
-
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
-});
-
-export const fetchMyAttempts = () => API.get("/attempts/my");
-export const fetchAttemptById = (attemptId) => {
-  return API.get(`/attempts/${attemptId}`);
-};
-
+export const fetchMyAttempts = () => api.get("/attempts/my");
+export const fetchAttemptById = (attemptId) => api.get(`/attempts/${attemptId}`);
